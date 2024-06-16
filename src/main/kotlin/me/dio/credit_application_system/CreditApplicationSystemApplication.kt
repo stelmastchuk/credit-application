66c67c8d67422d2d278336c67ccb01dda8ae1038
vstelmastchuk <vitorstelmastchuk@gmail.com>
2024-06-16 18:52:28 -0300

    first commit

---
+++ src/main/kotlin/me/dio/credit_application_system/CreditApplicationSystemApplication.kt
@@ -0,0 +1,11 @@
+package me.dio.credit_application_system
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CreditApplicationSystemApplication
+
+fun main(args: Array<String>) {
+	runApplication<CreditApplicationSystemApplication>(*args)
+}
---
+++ src/test/kotlin/me/dio/credit_application_system/CreditApplicationSystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.dio.credit_application_system
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CreditApplicationSystemApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
